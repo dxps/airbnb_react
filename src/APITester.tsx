239fbac99751bfbc62f8f2237ef4dfabba8998a5
dxps <marius.ileana@gmail.com>
2025-11-03 14:04:21 +0200

    add prettier
    And also ran it (using `bun run prettier`), that's why those updated source files.

--- src/APITester.tsx
+++ src/APITester.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import { useRef, type FormEvent } from "react";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { useRef, type FormEvent } from 'react';
 
 export function APITester() {
   const responseInputRef = useRef<HTMLTextAreaElement>(null);
@@ -14,9 +20,9 @@ export function APITester() {
     try {
       const form = e.currentTarget;
       const formData = new FormData(form);
-      const endpoint = formData.get("endpoint") as string;
+      const endpoint = formData.get('endpoint') as string;
       const url = new URL(endpoint, location.href);
-      const method = formData.get("method") as string;
+      const method = formData.get('method') as string;
       const res = await fetch(url, { method });
 
       const data = await res.json();
@@ -27,37 +33,43 @@ export function APITester() {
   };
 
   return (
-    <div className="flex flex-col gap-6">
-      <form onSubmit={testEndpoint} className="flex items-center gap-2">
-        <Label htmlFor="method" className="sr-only">
+    <div className='flex flex-col gap-6'>
+      <form onSubmit={testEndpoint} className='flex items-center gap-2'>
+        <Label htmlFor='method' className='sr-only'>
           Method
         </Label>
-        <Select name="method" defaultValue="GET">
-          <SelectTrigger className="w-[100px]" id="method">
-            <SelectValue placeholder="Method" />
+        <Select name='method' defaultValue='GET'>
+          <SelectTrigger className='w-[100px]' id='method'>
+            <SelectValue placeholder='Method' />
           </SelectTrigger>
-          <SelectContent align="start">
-            <SelectItem value="GET">GET</SelectItem>
-            <SelectItem value="PUT">PUT</SelectItem>
+          <SelectContent align='start'>
+            <SelectItem value='GET'>GET</SelectItem>
+            <SelectItem value='PUT'>PUT</SelectItem>
           </SelectContent>
         </Select>
-        <Label htmlFor="endpoint" className="sr-only">
+        <Label htmlFor='endpoint' className='sr-only'>
           Endpoint
         </Label>
-        <Input id="endpoint" type="text" name="endpoint" defaultValue="/api/hello" placeholder="/api/hello" />
-        <Button type="submit" variant="secondary">
+        <Input
+          id='endpoint'
+          type='text'
+          name='endpoint'
+          defaultValue='/api/hello'
+          placeholder='/api/hello'
+        />
+        <Button type='submit' variant='secondary'>
           Send
         </Button>
       </form>
-      <Label htmlFor="response" className="sr-only">
+      <Label htmlFor='response' className='sr-only'>
         Response
       </Label>
       <Textarea
         ref={responseInputRef}
-        id="response"
+        id='response'
         readOnly
-        placeholder="Response will appear here..."
-        className="min-h-[140px] font-mono resize-y"
+        placeholder='Response will appear here...'
+        className='min-h-[140px] resize-y font-mono'
       />
     </div>
   );
